2c772062be2824771d3d0c80c238eefc0ac48778
j-tee <juliustetteh@gmail.com>
2023-05-22 09:49:03 +0000

    Modified migration file
    Signed-off-by: j-tee <juliustetteh@gmail.com>

--- db/migrate/20230511174738_add_unconfirmed_email_to_user.rb
+++ db/migrate/20230511174738_add_unconfirmed_email_to_user.rb
@@ -1,5 +1,5 @@
 class AddUnconfirmedEmailToUser < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :unconfirmed_email, :string
+    # add_column :users, :unconfirmed_email, :string
   end
 end
