f4848dd652214dacb06832c561eb3c52172345da
j-tee <juliustetteh@gmail.com>
2023-05-23 15:54:33 +0000

    Update deploy file
    Signed-off-by: j-tee <juliustetteh@gmail.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -66,6 +66,12 @@ namespace :deploy do
 
   after :publishing, :restart
 end
+# set :default_env, {
+#   PATH: '$HOME/.rvm/gems/ruby-3.2.1/bin:$HOME/.rvm/gems/ruby-3.2.1@global/bin:$HOME/.rvm/rubies/ruby-3.2.1/bin:$PATH'
+# }
 set :default_env, {
-  PATH: '$HOME/.rvm/gems/ruby-3.2.1/bin:$HOME/.rvm/gems/ruby-3.2.1@global/bin:$HOME/.rvm/rubies/ruby-3.2.1/bin:$PATH'
+  PATH: '$HOME/.rvm/bin:$PATH',
+  RUBY_VERSION: 'ruby-3.2.1',
+  GEM_HOME: '$HOME/.rvm/gems/ruby-3.2.1',
+  GEM_PATH: '$HOME/.rvm/gems/ruby-3.2.1:$HOME/.rvm/gems/ruby-3.2.1@global'
 }
