90e500b4a303cc918f41c808a21554ad38c77ced
j-tee <juliustetteh@gmail.com>
2023-06-08 20:02:32 +0000

    Update cors file
    Signed-off-by: j-tee <juliustetteh@gmail.com>

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,22 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
+if Rails.env.development?
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'
 
-    resource '*',
-      headers: :any,
-      expose: ['access-token', 'expiry', 'token-type', 'Authorization'],
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+  end
+elsif Rails.env.production?
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'https://pos.sales-forge.com'
+
+      resource '*',
+        headers: :any,
+        expose: ['access-token', 'expiry', 'token-type', 'Authorization'],
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
   end
 end
