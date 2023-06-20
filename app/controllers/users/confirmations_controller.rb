cd7f313aac48e424089a3e75c6d3ceb6ab0b807e
j-tee <juliustetteh@gmail.com>
2023-06-20 17:02:35 +0000

    update
    Signed-off-by: j-tee <juliustetteh@gmail.com>

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -6,16 +6,16 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   respond_to :html
 
   private
-
   def respond_with(resource, _opts = {})
-    base_url = Rails.env.production? ? ENV['REACT_APP_BASE_URL'] : 'http://localhost:3001'
+    base_url = ENV['REACT_APP_BASE_URL']
   
     if resource.persisted?
-      redirect_to "#{base_url}/confirmation/?result=success", allow_other_host: true
+      redirect_to File.join(base_url, '/confirmation/?result=success'), allow_other_host: true
     else
-      redirect_to "#{base_url}/confirmation/?result=failure", allow_other_host: true
+      redirect_to File.join(base_url, '/confirmation/?result=failure'), allow_other_host: true
     end
-  end  
+  end
+  
 
   # def respond_with(resource, _opts = {})
   #   if resource.persisted?
